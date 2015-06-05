52bde01ab0f24f8ae922531f551ca901d965e5f7
chicofos <fco.ochoas@gmail.com>
2015-06-04 19:12:36 -0700

    cluster modifications and https added

--- server.js
+++ server.js
@@ -1,26 +1,15 @@
+var fs = require('fs');
+var https = require('https');
 var express = require('express');
 var app = express();
 var io = require('./realtime');
-var cluster = require('cluster');
+var favicon = require('serve-favicon');
+var morgan = require('morgan');
+
 
 var port = process.env.PORT || 3000;
 
-if(cluster.isMaster){
-    var i = 0;
-    //2 extra clusters
-    for (i; i < 3; i++){
-        console.log('Cluster %s running', ++i);
-     cluster.fork();
-    }
-
-    //if the worker dies, restart it.
-   cluster.on('exit', function(worker){
-      console.log('Worker ' + worker.id + ' died..');
-      cluster.fork();
-   });
-
-}
-else{
+  app.use(favicon(__dirname + '/public/favicon.ico'));
 
   //view engine
   app.engine('.html', require('ejs').__express);
@@ -30,22 +19,20 @@ else{
   app.use(express.static(__dirname + '/public'));
   app.use(express.static(__dirname + '/public/music'));
   app.use(express.static(__dirname + '/public/dist'));
+  app.use(morgan('combined'))
 
   //Routes
   var routes = require('./routes')(app);
   
   //Listen
-  var server = app.listen(port, onListen);
+   var server = https.createServer({
+      key: fs.readFileSync('key.pem'),
+      cert: fs.readFileSync('cert.pem')
+    }, app).listen(port, onListen);
+
 
   io(server);
 
-  process.on('uncaughtException', function(err){
-      console.log(err);
-      //Send some notification about the error  
-      process.exit(1);
-  });
-
-}
 
 function onListen(){
   console.log('Server running on port %s', port);
